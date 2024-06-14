d363680833c5f5175bd1ab3e1ca9dcaf938af9ef
joon <khjoon372@naver.com>
2024-06-14 15:28:11 +0900

    :sparkles: feat: 레시피 엔티티 작성

--- src/main/java/com/sm/project/domain/food/Recipe.java
+++ src/main/java/com/sm/project/domain/food/Recipe.java
@@ -2,6 +2,7 @@ package com.sm.project.domain.food;
 
 
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
@@ -14,12 +15,28 @@ public class Recipe {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "food_id")
+    @Column(name = "recipe_id")
     private Long id;
 
-    @Column(nullable = false, columnDefinition = "VARCHAR(20)")
-    private String name;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String name; // 이름
+
+    @Column(columnDefinition = "TEXT")
+    private String info; // 몇인분, 조리시간
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String description;
+    private String description;//레시피
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String ingredient;// 재료
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Long recommendCount;// 추천수
+
+    @Column(columnDefinition = "BOOLEAN")
+    @ColumnDefault("false")
+    private Boolean bookmark;
+
+
 }
